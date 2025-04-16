7f373e7f2bcb3c87335f4b3515af96e01235de17
vgonsalez <gonsalez@yahoo.com>
2025-04-16 18:25:36 -0300

    arquivo SessionService corrigido mas continua apresentando erro NullPointerException

--- src/main/java/com/voting/voting_api/repository/SessionRepository.java
+++ src/main/java/com/voting/voting_api/repository/SessionRepository.java
@@ -1,18 +1,19 @@
 package com.voting.voting_api.repository;
 
-import com.voting.voting_api.model.Session;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
+import com.voting.voting_api.model.Session;
 
 @Repository
 public interface SessionRepository extends JpaRepository<Session, Long> {
     Optional<Session> findByPautaIdAndClosedFalse(Long pautaId);
     
     @Query("SELECT s FROM Session s WHERE s.closed = false AND s.endTime <= :now")
-    List<Session> findSessionsToClose(LocalDateTime now);
+    List<Session> findSessionsToClose(LocalDateTime now);   
 }
