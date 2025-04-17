d83eeff4c1012337fd9f837dd834ef9bdf0e1815
vgonsalez <gonsalez@yahoo.com>
2025-04-16 21:03:05 -0300

    correção do erro em session service

--- src/main/java/com/voting/voting_api/VotingApiApplication.java
+++ src/main/java/com/voting/voting_api/VotingApiApplication.java
@@ -2,14 +2,14 @@ package com.voting.voting_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
-@EnableScheduling
-public class VotingApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(VotingApiApplication.class, args);
+@SpringBootApplication(scanBasePackages = {
+	    "com.voting.voting_api.repository",
+	    "com.voting.voting_api.service",
+	    "com.voting.voting_api.model"
+	})
+	public class VotingApiApplication {
+	    public static void main(String[] args) {
+	        SpringApplication.run(VotingApiApplication.class, args);
+	    }
 	}
-
-}
